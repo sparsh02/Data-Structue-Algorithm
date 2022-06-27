d97ba79a1cccf7e1cf1dc4b5a3bf5e9482d68ddb
Sparsh Mishra <sparshmishra02@gmail.com>
2022-06-27 18:58:05 +0530

    started greedy algo

---
+++ greedy-gfg/n number of meetings.cpp
@@ -0,0 +1,54 @@
+/*Notes :
+-> using a pair < pair <int, int> , int> to store the start, end and positon of given data
+-> sort that data according to a compare function u gonna have to make.
+-> acc to ques the start time of the second meeting should not be less or equal to prev meeting so according to that run loop and do res++
+-> return res
+*/
+
+
+
+
+public:
+
+static bool pairCompare(const pair<pair<int, int>, int> &a,
+                     const pair<pair<int, int>, int> &b) 
+    {
+        if (a.first.second != b.first.second) {
+            return a.first.second < b.first.second;
+        }
+        return a.second < b.second;
+    }
+    
+    
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n) 
+    {
+    
+        vector<pair<pair<int, int>, int>> x;
+     
+        //pushing the pair of starting and finish time and their 
+        //index as pair in another list.
+        for (int i = 0; i < n; i++) 
+        x.push_back({{start[i], end[i]}, i + 1});
+    
+        //sorting the list.
+        sort(x.begin(), x.end(), pairCompare);
+        int last = 0;
+        int res = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            //if the start time of this meeting is greater than or equal
+            //to the finish time of previously selected meeting then 
+            //we increment the counter and update last.
+            if (x[i].first.first > last) 
+            {
+                res++;
+                last = x[i].first.second;
+            }
+        }
+        //returning the counter.
+        return res;
+    }
+};
