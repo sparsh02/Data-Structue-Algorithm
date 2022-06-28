eaa49ff39a26ca0c4c7614ddf751c596f068d439
Sparsh Mishra <sparshmishra02@gmail.com>
2022-06-28 09:38:13 +0530

    minimum platforms

---
+++ greedy-gfg/minimum number of platform for trains.cpp
@@ -0,0 +1,38 @@
+/*
+Given arrival and departure times of all trains that reach a railway station. Find the minimum number of platforms required for the railway station so that no train is kept waiting.
+Consider that all the trains arrive on the same day and leave on the same day. Arrival and departure time can never be the same for a train but we can have arrival time of one train equal to departure time of the other. At any given instance of time, same platform can not be used for both departure of a train and arrival of another train. In such cases, we need different platforms.*/
+
+//using greedy algo
+//sorting both the array given and then simply checking for overlap while increasing the counter, and decreasing counter if no overlaps
+//storing max value of overlaps and returning that
+
+
+
+
+
+
+
+int countPlatforms(int n,int arr[],int dep[])
+ {
+    sort(arr,arr+n);
+    sort(dep,dep+n);
+ 
+    int ans=1;
+    int count=1;
+    int i=1,j=0;
+    while(i<n && j<n)
+    {
+        if(arr[i]<=dep[j]) //one more platform needed
+        {
+            count++;
+            i++;
+        }
+        else //one platform can be reduced
+        {
+            count--;
+            j++;
+        }
+        ans=max(ans,count); //updating the value with the current maximum
+    }
+    return ans;
+ }
